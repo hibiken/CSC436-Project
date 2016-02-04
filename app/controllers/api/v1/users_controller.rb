395d4d17c2ff310eebb7543b8e62d40727a563e4
Ken Hibino <ken.hibino7@gmail.com>
2016-02-04 08:46:33 -0500

    Add users#create action and corresponding endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user, status: 200
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email,
+                                   :password, :password_confirmation)
+    end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Api::V1::UsersController do
   describe "GET #show" do
     before(:each) do
       @user = create(:user)
-
       get :show, id: @user.id, format: :json
     end
 
@@ -17,6 +16,40 @@ RSpec.describe Api::V1::UsersController do
 
     it "responds with HTTP status 200" do
       expect(response.status).to eq(200)
+    end
+  end
+
+  describe "POST #create" do
+    context "creation successful" do
+      before(:each) do
+        @user_attributes = attributes_for(:user)
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the new user record in JSON format" do
+        user = JSON.parse(response.body, symbolize_names: true)
+        expect(user[:email]).to eq(@user_attributes[:email])
+      end
+
+      it "responds with HTTP status 201" do
+        expect(response.status).to eq(201)
+      end
+    end
+
+    context "creation unsuccessful" do
+      before(:each) do
+        @invalid_attributes = attributes_for(:user, email: " ")
+        post :create, { user: @invalid_attributes }, format: :json
+      end
+
+      it "renders json with error message" do
+        user = JSON.parse(response.body, symbolize_names: true)
+        expect(user[:errors]).to be_present
+      end
+
+      it "responds with HTTP status 422" do
+        expect(response.status).to eq(422)
+      end
     end
   end
 
