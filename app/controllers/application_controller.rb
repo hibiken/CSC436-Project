5a3830b6102be42646fcedc588ca2f5528d2156a
Ken Hibino <ken.hibino7@gmail.com>
2016-02-04 21:01:24 -0500

    Bug fix: fix current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  def current_user
-    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
-  end
+  protected
 
-  def authenticate_with_token!
-    unless current_user.present?
-      render json: { errors: "Not authorized" }, status: 401
+    def current_user
+      return if request.headers['Authorization'].nil?
+      @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
     end
-  end
 
-  def user_signed_in?
-    current_user.present?
-  end
+    def authenticate_with_token!
+      unless current_user.present?
+        render json: { errors: "Not authorized" }, status: 401
+      end
+    end
+
+    def user_signed_in?
+      current_user.present?
+    end
 end
