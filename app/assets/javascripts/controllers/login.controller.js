3b93dc825aeb7ae66c633b7d0318f5f9c00dd6d8
relli006 <relli006@plattsburgh.edu>
2016-02-18 10:22:06 -0500

    Finish setting up the login controller and created the main app.js file

---
+++ app/assets/javascripts/controllers/login.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+			/*************Autherticaation service is used for this so check authentication.service.js******************/
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+    }
+
+})();
