1e35e7b257d39bffbbba97c8711255a3148a9b4f
Ken Hibino <ken.hibino7@gmail.com>
2016-03-01 07:18:14 -0500

    Get rid of subdomain in the URL

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_with_token!, only: [:update]
 
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
   def show
     user = User.find(params[:id])
     render json: user, status: 200
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 require "api_constraints"
 
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json },
-                  constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create, :update] do
+      resources :users, only: [:index, :show, :create, :update] do
         resources :posts, only: [:show, :create, :update]
       end
       resources :sessions, only: [:create, :destroy]
