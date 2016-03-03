40aada04d96381ada6e0b07772403bc5b5557849
Ken Hibino <ken.hibino7@gmail.com>
2016-03-03 11:20:32 -0500

    Add home page for API documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require "api_constraints"
 
 Rails.application.routes.draw do
+  root 'home#show'
+
   namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:index, :show, :create, :update] do
